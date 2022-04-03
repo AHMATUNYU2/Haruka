f9d34741604cfa759990095cdb663f0670fde28b
AHMAT <101961123+AHMATUNYU2@users.noreply.github.com>
2022-04-03 19:57:50 +0700

    Config.js
    const fs = require('fs')
    const chalk = require('chalk')
    
    // self or public
    global.self = false //jadiin true klo gk mau fitur bot lu di pke sama org lain
    global.anticall = false
    global.anti_delete = false
    
    // setting
    global.ownername ="AHMAT UNYU2"
    global.ownernumber = "6282338699565"
    global.botname = "VISBot"
    global.thumbnail = fs.readFileSync("./settings/Haruka.jpg") //sesuaikan dengan nama foto
    global.background = "https://telegra.ph/file/d4c05638fa7886a1d8060.jpg"
    global.lolkey = 'e966d3aac613b61dabd35cc6' //register lolhuman.xyz klo mau apikey
    global.zenzkey = '8423e19c13' //register zenzapi.xyz klo mau apikey
    global.limit = {
    		free:20,
    		premium:1000
    	}
    global.session_name = "session.json"
    
    
    let file = require.resolve(__filename)
    fs.watchFile(file, () => {
    	fs.unwatchFile(file)
    	console.log(chalk.redBright(`Update'${__filename}'`))
    	delete require.cache[file]
    	require(file)
    })

--- settings/config.js
+++ settings/config.js
@@ -1,3 +1,14 @@
+
+
+
+
+
+
+
+
+
+
+
 const fs = require('fs')
 const chalk = require('chalk')
 
@@ -7,9 +18,9 @@ global.anticall = false
 global.anti_delete = false
 
 // setting
-global.ownername ="ZeeoneOfc"
-global.ownernumber = "62887435047326"
-global.botname = "Haruka-Bot"
+global.ownername ="AHMAT UNYU2"
+global.ownernumber = "6282338699565"
+global.botname = "VISBot"
 global.thumbnail = fs.readFileSync("./settings/Haruka.jpg") //sesuaikan dengan nama foto
 global.background = "https://telegra.ph/file/d4c05638fa7886a1d8060.jpg"
 global.lolkey = 'e966d3aac613b61dabd35cc6' //register lolhuman.xyz klo mau apikey
@@ -28,3 +39,20 @@ fs.watchFile(file, () => {
 	delete require.cache[file]
 	require(file)
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
